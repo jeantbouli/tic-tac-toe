603b6cd8431696e862477333cac46358423e7832
jeantbouli <johntbouli17@gmail.com>
2024-06-26 14:15:23 +0200

    a

--- helpers.py
+++ helpers.py
@@ -1,30 +1,32 @@
-    # Drawing the board on the console and using the \n command to move to the next line
-    # Giving our function an input of spots
-    # Mapping the keys into the board
+# Drawing the board on the console and using the \n command to move to the next line
+# Function to draw the Tic-Tac-Toe board based on the spots
 def draw_board(spots):
     board = (f"|{spots[1]}|{spots[2]}|{spots[3]}|\n"
              f"|{spots[4]}|{spots[5]}|{spots[6]}|\n"
              f"|{spots[7]}|{spots[8]}|{spots[9]}|")
     print(board)
 
+# Function to determine whose turn it is (X or O) based on the turn number
 def check_turn(turn):
-    #We use the modulo operator to find out if the turn is even or odd and dub each turn with it's relative symbol
-    if turn % 2 == 0: return "O"
-    else: return "X"
-#We need to determine if a player has won, and there are 8 such cases , 2 diagnoally, 3 vertically, 3 horizontally 
+    if turn % 2 == 0:
+        return "O"
+    else:
+        return "X"
+
+# Function to check if a player has won by examining the spots on the board
 def check_for_win(spots):
     if   (spots[1] == spots[2] == spots[3]) \
       or (spots[4] == spots[5] == spots[6]) \
       or (spots[7] == spots[8] == spots[9]):
-      return True
-  # Handle Vertical Cases
+        return True
+    # Handle Vertical Cases
     elif   (spots[1] == spots[4] == spots[7]) \
       or (spots[2] == spots[5] == spots[8]) \
       or (spots[3] == spots[6] == spots[9]):
-      return True
-  # Diagonal Cases
+        return True
+    # Diagonal Cases
     elif (spots[1] == spots[5] == spots[9]) \
       or (spots[3] == spots[5] == spots[7]):
-      return True
-    
-    else: return False
+        return True
+    else:
+        return False
--- main.py
+++ main.py
@@ -1,55 +1,54 @@
-from helpers import draw_board,check_turn, check_for_win
-#We dont want the board to be redrawn everytime the player inputs a variable
-import os 
-#we use a dictionary to have a unique identifier that can be used to access or modify data
+from helpers import draw_board, check_turn, check_for_win
+import os
 
-spots = {1 : '1', 2 : '2', 3: '3', 4 : '4', 5 : '5', 
-         6 : '6', 7 : '7',  8 : '8', 9 : '9'}
+# Dictionary to store the spots on the board with unique identifiers
+spots = {1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9'}
 
-#Since Games may very in length, with some ending quickly and some lasting as long as possible, the best option for knowing how many times the player needs to update the board is to use a while loop
+# Game loop to keep the game running until completion
 playing = True
-#By defaul, the game isnt won so we set the "Completion" of it to false
 complete = False
-#If a player enters a number from 1 to 9 we want to put an X or O on that spot, we need to keep track of who's turn it is 
+
+# Variable to track the player's turn
 turn = 0
 prev_turn = -1
+
 while playing:
-    #we use a ternary operator to check what the operating system is to pass the right command and clear the screen
     os.system("cls" if os.name == "nt" else "clear")
-    #We begin by drawing the board
     draw_board(spots)
-    #Let the player know if an invalid spot is selected 
+
+    # Prompting the player for their turn
     if prev_turn == turn:
         print("Invalid Spot Selected, Please Pick Another")
     prev_turn = turn
-    print("Player " + str((turn % 2) +1 ) + "'s turn: Pick your spot or press q to quit")
-    #Once the board has been drawn, its time to get input from the player
+    print("Player " + str((turn % 2) + 1) + "'s turn: Pick your spot or press q to quit")
+
     choice = input()
+
     if choice == "q":
         playing = False
-    #to check that the input is a number not a letter     and to convert the input to an integer and check if its one of they keys in the spots dictionary
     elif str.isdigit(choice) and int(choice) in spots:
-        """we need to solve the issue of overriding a previously reserved spot on the board
-        first we're going to want to increment the turn by one and then next we're going to want to pass
-        turn into our check turn function which will return either an x or o depending on whose turn it is we're going to
-        replace the value of whatever our player gives us the number one through nine on 
-        the spot's dictionary with that x or o
-        """
+        # Logic for updating the board with player's choice
         if not spots[int(choice)] in {"X", "O"}:
             turn += 1
             spots[int(choice)] = check_turn(turn)
 
-    if check_for_win(spots): playing, complete = False, True
-    #Handling Ties1
-    if turn > 8: playing = False
-    
+    if check_for_win(spots):
+        playing, complete = False, True
 
-#Printing the Results, Draw the Board One Last Time
+    # Checking for a tie
+    if turn > 8:
+        playing = False
+
+# End of game, displaying results
 os.system("cls" if os.name == "nt" else "clear")
 draw_board(spots)
+
 if complete:
-    if check_turn(turn) == "X": print("Player 1 IS THE WINNER")
-    else: print("Player 2 IS THE WINNER")
+    # Determining the winner or a tie
+    if check_turn(turn) == "X":
+        print("Player 1 IS THE WINNER")
+    else:
+        print("Player 2 IS THE WINNER")
 else:
     print("YOU TIED")
 
